3725bd936581cf7c16f49359c06c0165a41dc408
superflem <alex.flem@hotmail.it>
2022-06-19 15:46:57 +0200

    preparato l'invio dei file

--- frontend/src/js/Home.js
+++ frontend/src/js/Home.js
@@ -1,20 +1,30 @@
 import '../css/Home.css';
+import axios from 'axios';
+axios.defaults.withCredentials = true;
 
 const Home = (props) => {
     const nome = props.nome;
 
-    //const nome = "Alex";
+    const handleLoginForm = async (e) => {
+
+    }
+
     return (
         <div className="divHome">
             <h1>Benvenuto</h1>
             <h2>{nome}</h2>
+
+            <form onSubmit = {handleLoginForm}>
+                <input type="file" />
+                <button id='bottone' type='submit'> Invia</button>
+            </form>
 
             
         </div> 
         
 
 
-     );
+    );
 }
  
 export default Home;
